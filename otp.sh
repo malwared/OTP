9c5f0f9e619a6c994e60b48e66de56bd0fe67fdd
TheMalwared <malwared@users.noreply.github.com>
2016-10-15 13:06:56 +0400

    Update otp.sh

--- otp.sh
+++ otp.sh
@@ -2,5 +2,4 @@
 Ktime=$(date | awk {'print $4'} | awk -F ":" {'print $1":"$2'})
 key=$Ktime"VeryStrongKey"
 passw0rd=$(echo -n $key | md5sum | awk {'print $1'} | awk '{print substr($0,20)}')
-echo $passw0rd
 echo -e "$passw0rd\n$passw0rd" | (passwd UserName)
